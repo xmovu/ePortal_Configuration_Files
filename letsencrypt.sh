3ebdd4ea8c8334cd467bfbc800b4d5474f457f5c
Miguel Varela <mail@miguelsp.net>
2023-03-10 18:06:24 +0000

    Better handle line detetion.

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -21,9 +21,10 @@ echo Installing the SSL..
 certbot certonly --webroot --webroot-path /usr/share/nginx/html -m $varEmail --agree-tos -d $varDomain --key-type ecdsa --elliptic-curve secp384r1
 echo Configuring nginx...
 mv /etc/nginx/eportal.ssl.conf.example /etc/nginx/eportal.ssl.conf
-sed -i -e 's|server_name.*|server_name $varDomain;|g' /etc/nginx/eportal.ssl.conf
-sed -i -e 's|ssl_certificate.*|ssl_certificate /etc/letsencrypt/live/$varDomain/fullchain.pem;|g' /etc/nginx/eportal.ssl.conf
-sed -i -e 's|ssl_certificate_key.*|ssl_certificate_key /etc/letsencrypt/live/$varDomain/privkey.pem;|g' /etc/nginx/eportal.ssl.conf
+grep -c "include eportal.ssl.conf;" /etc/nginx/conf.d/eportal.conf || sed -i "3i \ include eportal.ssl.conf;" /etc/nginx/conf.d/eportal.conf
+sed -i -e "s|server_name.*|server_name $varDomain;|g" /etc/nginx/eportal.ssl.conf
+sed -i -e "s|ssl_certificate .*|ssl_certificate /etc/letsencrypt/live/$varDomain/fullchain.pem;|" /etc/nginx/eportal.ssl.conf
+sed -i -e "s|ssl_certificate_key .*|ssl_certificate_key /etc/letsencrypt/live/$varDomain/privkey.pem;|" /etc/nginx/eportal.ssl.conf
 service nginx restart
 echo Done!
 echo You may visit your ePortal at https://$varDomain
