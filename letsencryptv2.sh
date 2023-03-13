137861a5ebedaa93700cdb475b9a6b7fd5eddd56
Miguel Varela <mail@miguelsp.net>
2023-03-13 16:35:22 +0000

    Update letsencryptv2.sh

--- letsencryptv2.sh
+++ letsencryptv2.sh
@@ -11,11 +11,39 @@ echo "Enter the domain for the SSL Certificate."
 read varDomain
 echo Hostname selected: $varDomain, proceeding to install certbot...
 sleep 3
+# Determine the Linux distribution
+if [ -f /etc/redhat-release ]; then
+  DISTRIBUTION="Red Hat"
+elif [ -f /etc/lsb-release ]; then
+  DISTRIBUTION="Ubuntu"
+  UBUNTU_VERSION=$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d "=" -f 2)
+fi
+
+# Execute commands based on the Linux distribution
+if [ "$DISTRIBUTION" = "Red Hat" ]; then
+  # Execute commands for Red Hat
 yum install snapd
 systemctl enable --now snapd.socket
 snap install core; snap refresh core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
+  # Add your commands here for Red Hat
+elif [ "$DISTRIBUTION" = "Ubuntu" ]; then
+  # Execute commands for Ubuntu
+  if [ "$(echo $UBUNTU_VERSION'<'20.04 | bc -l)" -eq 0 ] && [ "$(echo $UBUNTU_VERSION'<'22.04 | bc -l)" -eq 1 ]; then
+    # Execute commands for Ubuntu 20.04
+snap install core; snap refresh core
+snap install --classic certbot
+    # Add your commands here for Ubuntu 20.04
+  else
+    # Execute commands for Ubuntu 22.04 or later
+snap install core; snap refresh core
+snap install --classic certbot
+    # Add your commands here for Ubuntu 22.04 or later
+  fi
+else
+  echo "Unsupported distribution detected"
+fi
 echo Issuing the SSL, but first, please enter your email address.
 read varEmail
 echo Email address: $varEmail
